c5e6b4d57700b96b564396d38ba8db5ec5a15b1d
Ivan Kerin <ikerin@gmail.com>
2024-10-15 10:28:47 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,16 +3,11 @@ set -ex
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install vim grc
+sudo apt-get install vim
 
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
 	echo "Installing vim plug"
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-if [ ! -f ~/.grc/.grc.bashrc ]; then
-	echo "Installing grc bashrc"
-	curl -fLo ~/.grc/.grc.bashrc --create-dirs https://raw.githubusercontent.com/garabik/grc/master/grc.bashrc
 fi
 
 if command -v terraform >&2; then
